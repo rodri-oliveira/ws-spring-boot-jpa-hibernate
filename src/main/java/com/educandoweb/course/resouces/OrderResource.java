ee71a3c0c9fd567447879ed950bd4a7f88f6f8bd
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2024-06-13 11:50:01 -0300

    Order, Instant, ISO 8601 - part 2

--- src/main/java/com/educandoweb/course/resouces/OrderResource.java
+++ src/main/java/com/educandoweb/course/resouces/OrderResource.java
@@ -9,27 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.User;
-import com.educandoweb.course.services.UserService;
-
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/orders")
+public class OrderResource {
 	
 	@Autowired
-	private UserService userService;
+	public OrderService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> list = userService.findAll();
+	public ResponseEntity<List<Order>> findAll(){
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User obj = userService.findById(id);
+	public ResponseEntity<Order> findById(@PathVariable Long id){
+		var obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
 }
